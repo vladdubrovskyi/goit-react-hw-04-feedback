c4bbd899a009bf85d77bc643e2b7e65441e55ff7
vladdubrovskyi <vladdubrovskyi1993@gmail.com>
2022-10-30 16:35:38 +0200

    feedback on hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,33 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import {Section} from "components/Section/Section"
 import {FeedbackVariants} from "components/FeedbackVariants/FeedbackVariants"
 import {StatisticsData} from "components/StatisticsData/StatisticsData"
 import {Notification} from "components/Notification/Notification"
-export class App extends Component {
-  state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-  }
 
-  btnHandler = e => {
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+ const btnHandler = e => {
     const { name } = e.target;
-    this.setState(ps => {
+    setGood(ps => {
       return { [name]: ps[name] + 1 };
     });
+   setNeutral(ps => {
+      return { [name]: ps[name] + 1 };
+   });
+   setBad(ps => {
+      return { [name]: ps[name] + 1 };
+   });
   }
-  
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <>
+
+
+  return (  <>
         <Section title = "Please leave feedback">
           
-          <FeedbackVariants variants={Object.keys(this.state)}
-          onClick = {this.btnHandler}>
+          <FeedbackVariants variants={[good, neutral, bad]}
+          onClick = {btnHandler}>
             
           </FeedbackVariants>
         </Section>
@@ -39,7 +42,47 @@ export class App extends Component {
           /> : <Notification/>
          }
        </Section>
-      </>
-    )
-  }
-};
+      </>)
+}
+
+
+// export class App extends Component {
+//   state = {
+//   good: 0,
+//   neutral: 0,
+//   bad: 0
+//   }
+
+//   btnHandler = e => {
+//     const { name } = e.target;
+//     this.setState(ps => {
+//       return { [name]: ps[name] + 1 };
+//     });
+//   }
+  
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     return (
+//       <>
+//         <Section title = "Please leave feedback">
+          
+//           <FeedbackVariants variants={Object.keys(this.state)}
+//           onClick = {this.btnHandler}>
+            
+//           </FeedbackVariants>
+//         </Section>
+//         <Section title = "Statistics">
+//           { (good + bad + neutral)>0 ?
+//           <StatisticsData
+//             good={good}
+//             neutral={neutral}
+//             bad={bad}
+//             total={good + bad + neutral}
+//             positivePercentage = {((good*100)/ (good + bad + neutral)).toFixed(2) }
+//           /> : <Notification/>
+//          }
+//        </Section>
+//       </>
+//     )
+//   }
+// };
